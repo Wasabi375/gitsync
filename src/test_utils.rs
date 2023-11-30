7622ff19311c4f1adf6cbab0957daac2d1b91cf1
Burkhard Mittelbach <wasabi37a@googlemail.com>
2023-11-30 22:51:22 +0100

    Fix test bug

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,12 +1,22 @@
-use std::{path::PathBuf, time::SystemTime};
+use std::{
+    fs,
+    path::PathBuf,
+    sync::Mutex,
+    thread::sleep,
+    time::{Duration, SystemTime},
+};
 
 use fs_extra::dir::{self, CopyOptions};
 use test_dir::{DirBuilder, TestDir};
 
+static TEST_DIR_MUTEX: Mutex<()> = Mutex::new(());
+
 pub fn test_dir(name: &str) -> TestDir {
+    let _lock = TEST_DIR_MUTEX.lock();
     let mut path = PathBuf::from(name);
     if path.exists() {
         path = PathBuf::from(format!("{name}{}", get_sys_time_in_secs()));
+        sleep(Duration::from_secs(1));
     }
 
     TestDir::current(path.to_str().expect("could not create test dir"))
@@ -19,20 +29,23 @@ pub fn existing_test_repo(name: &str) -> TestDir {
     assert!(existing.exists() && existing.is_dir());
 
     let test_dir = test_dir(&format!("existing_{name}"));
-    dir::copy(existing, test_dir.root(), &CopyOptions::new())
-        .expect("Failed to copy existing repo to temp test location");
+    dir::copy(
+        existing,
+        test_dir.root(),
+        &CopyOptions::new().content_only(true),
+    )
+    .expect("Failed to copy existing repo to temp test location");
 
     for git_dir in dir::get_dir_content(test_dir.root())
         .expect("Failed to read created test dir")
         .directories
         .iter()
-        .filter(|it| it.as_str() == "git-sync-repo")
+        .filter(|it| it.as_str().ends_with("git-sync-repo"))
     {
         let from = PathBuf::from(&git_dir);
         let mut to = from.parent().unwrap().to_path_buf();
         to.push(".git");
-        dir::move_dir(from, to, &CopyOptions::new())
-            .expect("Faileld to move git-sync-repo to .git in temp test dir");
+        fs::rename(from, to).expect("Faileld to move git-sync-repo to .git in temp test dir");
     }
 
     test_dir
